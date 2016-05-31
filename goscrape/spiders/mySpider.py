4de8044f01babb473e2aad34972584fae2051d5f
kiarash97 <kasirikiarash@gmail.com>
2016-05-31 15:11:18 +0430

    add app icon & author link & categori link

--- goscrape/spiders/mySpider.py
+++ goscrape/spiders/mySpider.py
@@ -7,8 +7,7 @@ class CafeBazaarSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["cafebazaar.ir"]
     start_urls = [
-        "https://cafebazaar.ir/cat/?l=en&partial=true"
-        # "https://cafebazaar.ir/app/com.blackstarteam.DoX/?l=en"
+        "https://cafebazaar.ir/cat/?l=fa&partial=true"
     ]
     url_global = ""
 
@@ -17,12 +16,13 @@ class CafeBazaarSpider(scrapy.Spider):
         filename = response.url.split("/")[-2] + '.html'
 
         for url in response.xpath('//a/@href').extract():
-            yield {"status": "cat", "item": {'slug': url.split('/')[-2]}}
-
+            cat_name=''.join(response.xpath('//a/span/text()').extract())
+            yield {"status": "cat", "item": {'slug': url.split('/')[-2],'name':cat_name}}
         for url in response.xpath('//a/@href').extract():
             yield scrapy.Request('https://cafebazaar.ir' + url, callback=self.find_more_app)
 
     def find_more_app(self, response):
+        #find the more app button in the page
         for url in response.xpath('//a/@href').extract():
             if ("new" in url) and ("best" not in url) and ("list" in url):
                 url = url.replace(" ", "")
@@ -31,6 +31,7 @@ class CafeBazaarSpider(scrapy.Spider):
                 yield scrapy.Request('https://cafebazaar.ir' + url + '&partial=true', callback=self.find_all_app)
 
     def find_all_app(self, response):
+        #find all apps data
         i = 0
         urls = response.xpath('//a/@href').extract()
         while True:
@@ -47,6 +48,7 @@ class CafeBazaarSpider(scrapy.Spider):
             i += 1
 
     def see_app_details(self, response):
+        #find all details of apps
         dic = {}
         category = response.xpath("//span[@itemprop='applicationSubCategory']/text()").extract_first()
         version = ''.join(response.xpath("//span[@itemprop='softwareVersion']/text()").extract())
@@ -57,31 +59,39 @@ class CafeBazaarSpider(scrapy.Spider):
         rate = ''.join(response.xpath("//div[@class='rating-fill']/@style").extract())
         name = ''.join(response.xpath("//h1[@itemprop='name']/text()").extract())
         author = ''.join(response.xpath("//div[@itemprop='author']/a/span/text()").extract())
-        component = response.url.split("/")[-2]
+        try:
+            dic['author_link']= ''.join(response.xpath("//div[@itemprop='author']/a/@href").extract()).split('/')[-2]
+        except:
+            pass
+        category_link=''.join(response.xpath("//span[@class='pull-right']/a/@href").extract())
         icon = ''.join(response.xpath("//img[@class='app-img']/@src").extract())
+        component=response.url.split("/")[-2]
 
         version = version.replace("\n", "")
         version = version.strip()
-        size = size.replace("\n", "")
-        size = size.strip()
-        act_install = act_install.replace("less than ", "")
-        act_install = act_install.replace(",", "")
-        act_install = act_install.replace("+", "")
+        size=size.replace("\n","")
+        size=size.strip()
+        act_install=act_install.replace("less than ","")
+        act_install=act_install.replace(",","")
+        act_install=act_install.replace("+","")
         removeDict = {'width': 10, '100%': 1, '80%': 1, '60%': 1, '40%': 1, '20%': 1}
         for i in removeDict:
             rate = rate.replace(i, '', removeDict[i])
-            rate = rate.replace(':', '')
-        rate = rate.strip()
-        rate = rate.replace("\n", "")
-        rate = rate.replace("%", "")
-        rate = int(rate)
+            rate=rate.replace(':','')
+
+        rate=rate.strip()
+        rate=rate.replace("\n","")
+        rate=rate.replace("%","")
+        rate=int(rate)
         name = name.replace('\n', '')
         name = name.strip()
         icon = icon.replace('\n','')
         icon = icon.strip()
 
-        dic['category'] = category
+        dic['category_name'] = category
+        dic['category_link']=category_link.split("/")[-2]
         dic['version'] = version
+
         try:
             dic['act_install'] = int(act_install)
         except(ValueError):
@@ -89,7 +99,11 @@ class CafeBazaarSpider(scrapy.Spider):
         dic['size'] = int(size)
         dic['price'] = int(price)
         dic['name'] = name
-        dic['author'] = author
-        dic['component'] = component
+        dic['author']=author
+        dic['component']=component
         dic['icon'] = icon
+        print "******"
+        print dic
+        print "*******"
+        input()
         yield {"status": "app", 'item': dic}
